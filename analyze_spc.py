562889d1d1b73a1ced709cca0ea42cd3296c3d37
Illidanz <illidanrulez@gmail.com>
2019-08-22 10:44:19 +0200

    Improved analyze_spc

--- analyze_spc.py
+++ analyze_spc.py
@@ -5,23 +5,19 @@ import common
 infolder = "work_NFP/SPC.NFP/" if "-p" in sys.argv else "extract_NFP/SPC.NFP/"
 outfile = "analyze_spc.txt"
 functions = {}
+common.loadTable()
+inversetable = {}
+for bigram, code in common.table.items():
+    inversetable[code] = bigram
 
 
 def writeLine(out, pos, byte, line):
     pos -= 16
-    if pos < 10000:
-        out.write("0")
-    if pos < 1000:
-        out.write("0")
-    if pos < 100:
-        out.write("0")
-    if pos < 10:
-        out.write("0")
     function = ""
     if pos in functions:
         function = functions[pos] + "  "
         del functions[pos]
-    out.write(str(pos) + " 0x" + common.toHex(byte) + ": " + line + " " + function + "\n")
+    out.write(str(pos).zfill(5) + " 0x" + common.toHex(byte) + ": " + line + " " + function + "  \n")
 
 
 def readBytes(f, n):
@@ -32,7 +28,7 @@ def readBytes(f, n):
 
 
 with codecs.open(outfile, "w", "utf-8") as out:
-    out.write(sys.argv[1] + "\n")
+    out.write(sys.argv[1] + "  \n")
     with open(infolder + sys.argv[1], "rb") as f:
         f.seek(12)  # "SCRP" + filesize + "CODE"
         codesize = common.readInt(f)
@@ -61,14 +57,35 @@ with codecs.open(outfile, "w", "utf-8") as out:
                 if byte == 0x10:
                     line = readBytes(f, 2)
                     f.seek(-2, 1)
-                    sjis = common.readShiftJIS(f)
-                    if sjis != "":
-                        line += "\"" + sjis + "\" "
+                    convert = ""
+                    if "-p" in sys.argv:
+                        sjislen = common.readUShort(f)
+                        try:
+                            i = 0
+                            while i < sjislen - 1:
+                                strbyte = common.readByte(f)
+                                if strbyte in common.codes:
+                                    convert += "<" + common.toHex(strbyte) + ">"
+                                    i += 1
+                                else:
+                                    f.seek(-1, 1)
+                                    char = common.toHex(common.readUShort(f))
+                                    convert += inversetable[char[-2:] + char[:-2]]
+                                    i += 2
+                        except KeyError:
+                            convert = ""
+                    if convert != "":
+                        line += "\"" + convert + "\" "
                     else:
                         f.seek(pos + 1)
-                        len = common.readUShort(f)
-                        asciistr = f.read(len - 1)
-                        line += "\"" + asciistr.decode("ascii").replace("\r", "").replace("\n", "") + "\" "
+                        sjis = common.readShiftJIS(f)
+                        if sjis != "":
+                            line += "\"" + sjis + "\" "
+                        else:
+                            f.seek(pos + 1)
+                            asciilen = common.readUShort(f)
+                            asciistr = f.read(asciilen - 1)
+                            line += "\"" + asciistr.decode("ascii").replace("\r", "").replace("\n", "") + "\" "
                     line += readBytes(f, 9)
                     writeLine(out, pos, byte, line)
                 elif byte == 0x15:
@@ -83,4 +100,4 @@ with codecs.open(outfile, "w", "utf-8") as out:
                 else:
                     writeLine(out, pos, byte, "Unknown!")
             for k, v in functions.items():
-                out.write("Missing function pointer " + str(k) + ": " + v + "\n")
+                out.write("Missing function pointer", k, ":", v, " \n")
