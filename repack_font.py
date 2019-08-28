7e8854d68fec7d3e2fa98a078721d48c850b8d56
Phantom <54214467+PhantomandGhost@users.noreply.github.com>
2019-08-28 10:48:28 +0100

    Add files via upload
    Removed full-sized 'DOTS' glyphs

--- repack_font.py
+++ repack_font.py
@@ -20,7 +20,7 @@ upperchars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 lowerchars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 punctuation = [" ", "!", "?", "'", "\"", "“", "”", ",", ".", ":", ";", "(", ")", "-", "+", "*", "&", "↓"]
-customs = ["STAR", "NOTE", "PERC", "WAVE", "DOT3", "DOT4", "DOT2"]
+customs = ["STAR", "NOTE", "PERC", "WAVE"]
 all = upperchars + lowerchars + numbers + punctuation + customs
 
 # X Position in the font.png file
